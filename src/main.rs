973c95b121165762916cc78781b8e5b2d016041c
MaloWinrhy <malo@arkada.fr>
2024-09-13 11:02:13 +0200

    feat: :sparkles: Add update_people

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,5 @@
 use bevy::prelude::*;
 
-fn main() {
-    App::new()
-        .add_systems(Startup, add_people)
-        .add_systems(Update, hello_world)
-        .run();
-}
-
-fn hello_world() {
-    println!("hello world!");
-}
 
 #[derive(Component)]
 struct Person;
@@ -17,8 +7,30 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
+fn main() {
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, (update_people).chain()))
+        .run();
+}
+
+fn hello_world(query: Query<&Name>) {
+    for name in query.iter() {
+        println!("Hello, {}!", name.0);
+    }
+}
+
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
     commands.spawn((Person, Name("Renzo Hume".to_string())));
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Elaina Proctor" {
+            name.0 = "Elaina Hume".to_string();
+            break; // We don't need to change any other names.
+        }
+    }
 }
